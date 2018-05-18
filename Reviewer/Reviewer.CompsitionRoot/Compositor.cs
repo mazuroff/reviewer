9e1e2a5b11633b5ee9522e5a1b3bc65dd8dd900d
Oleh Mazurkevych <o.mazurkevych@artygeek.com>
2018-05-18 12:57:50 +0300

    enabled DI

---
+++ Reviewer/Reviewer.CompsitionRoot/Compositor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Reviewer.DAL.Infrastructure;
+using Reviewer.DAL.Interfaces;
+using System;
+
+namespace Reviewer.CompsitionRoot
+{
+    public class Compositor
+    {
+        public void Compose(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<IUnitOfWork, UnitOfWork>();
+            serviceCollection.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+        }
+    }
+}
